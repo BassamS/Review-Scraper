a31cc91fef08484159cc40e846972f34a1f81bbe
Bassam AL SAEDY <basaidi@gmail.com>
2022-09-20 06:39:40 +0200

    final commit

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import time
 import pandas as pd
 from comment_scraper import AmazonScraper
+
+reviews = []
+amz_scraper = AmazonScraper()
+
+product_url = 'https://www.amazon.com/Big-Book-Dashboards-Visualizing-Real-World/product-reviews/1119282713/ref=cm_cr_arp_d_paging_btm_next_2?ie=UTF8&reviewerType=all_reviews&pageNumber=2'
+
+for page_num in range(2, 6):
+    reviews.extend(amz_scraper.scrapeReviews(
+        url=product_url, page_num=page_num))
+    time.sleep(1)
+
+df = pd.DataFrame(reviews)
+df.to_excel('amazon product review ({0}).xlsx'.format(
+    reviews[0].product_name), index=False)
